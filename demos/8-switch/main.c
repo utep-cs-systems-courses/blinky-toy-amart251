05652918138537d7ce732706d8bc51abb6641523
amart251 <amartinez251@miners.utep.edu>
2023-07-25 13:06:27 -0600

    undid changes to switch demo

--- demos/8-switch/main.c
+++ demos/8-switch/main.c
@@ -5,20 +5,20 @@
 #define LED_GREEN BIT6             // P1.6
 #define LEDS (BIT0 | BIT6)
 
-#define SW2 BIT1		/* switch1 is p1.3 */
-#define SWITCHES SW2		/* only 1 switch on this board */
+#define SW1zz BIT3		/* switch1 is p1.3 */
+#define SWITCHES SW1		/* only 1 switch on this board */
 
 void main(void) 
 {  
   configureClocks();
 
-  P2DIR |= LEDS;
-  P2OUT &= ~LEDS;		/* leds initially off */
+  P1DIR |= LEDS;
+  P1OUT &= ~LEDS;		/* leds initially off */
   
-  P2REN |= SWITCHES;		/* enables resistors for switches */
-  P2IE |= SWITCHES;		/* enable interrupts from switches */
-  P2OUT |= SWITCHES;		/* pull-ups for switches */
-  P2DIR &= ~SWITCHES;		/* set switches' bits for input */
+  P1REN |= SWITCHES;		/* enables resistors for switches */
+  P1IE |= SWITCHES;		/* enable interrupts from switches */
+  P1OUT |= SWITCHES;		/* pull-ups for switches */
+  P1DIR &= ~SWITCHES;		/* set switches' bits for input */
 
   or_sr(0x18);  // CPU off, GIE on
 } 
@@ -26,28 +26,28 @@ void main(void)
 void
 switch_interrupt_handler()
 {
-  char p2val = P2IN;		/* switch is in P1 */
+  char p1val = P1IN;		/* switch is in P1 */
 
 /* update switch interrupt sense to detect changes from current buttons */
-  P2IES |= (p2val & SWITCHES);	/* if switch up, sense down */
-  P2IES &= (p2val | ~SWITCHES);	/* if switch down, sense up */
+  P1IES |= (p1val & SWITCHES);	/* if switch up, sense down */
+  P1IES &= (p1val | ~SWITCHES);	/* if switch down, sense up */
 
 /* up=red, down=green */
-  if (p2val & SW2) {
-    P2OUT |= LED_RED;
-    P2OUT &= ~LED_GREEN;
+  if (p1val & SW1) {
+    P1OUT |= LED_RED;
+    P1OUT &= ~LED_GREEN;
   } else {
-    P2OUT |= LED_GREEN;
-    P2OUT &= ~LED_RED;
+    P1OUT |= LED_GREEN;
+    P1OUT &= ~LED_RED;
   }
 }
 
 
 /* Switch on P1 (S2) */
 void
-__interrupt_vec(PORT2_VECTOR) Port_2(){
-  if (P2IFG & SWITCHES) {	      /* did a button cause this interrupt? */
-    P2IFG &= ~SWITCHES;		      /* clear pending sw interrupts */
+__interrupt_vec(PORT1_VECTOR) Port_1(){
+  if (P1IFG & SWITCHES) {	      /* did a button cause this interrupt? */
+    P1IFG &= ~SWITCHES;		      /* clear pending sw interrupts */
     switch_interrupt_handler();	/* single handler for all switches */
   }
 }
